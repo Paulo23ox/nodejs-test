2487bc4b779e08037167f890287ec8f31f329a2b
Paulo23ox <112489432+Paulo23ox@users.noreply.github.com>
2023-05-12 19:46:45 +0000

    melhoria no codigo

---
+++ app.js
@@ -0,0 +1,11 @@
+const leia = require('mclain-cli-prompt')
+ 
+const nome = leia('Digite seu nome'):;
+const idade = leia('Digite sua idade:');
+
+ console.log(
+    [ Cartão de aniversario]
+    
+    Olá $(nome)!
+
+ )
